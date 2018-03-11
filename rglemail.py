d64b311b1989b7bf7a0721cf0f20e71018a01abe
Nett18 <kressckerl@gmail.com>
2018-03-11 20:34:55 +0100

    Fixed sloppy code

--- rglemail.py
+++ rglemail.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import imaplib, smtplib, getpass, os
+try:
+    input = raw_input
+except:
+    pass
 variable1 = 0
 if os.path.exists("error.txt"):
     os.remove("error.txt")
 var1 = raw_input("Please enter your email: ")
-print "The following text is hidden, but it is still typed."
+print("The following text is hidden, but it is still typed.")
 var2 = getpass.getpass()
 mserver = imaplib.IMAP4_SSL \
 	('imap.gmail.com',993)
@@ -17,23 +21,28 @@ stat, dta = mserver.fetch \
 			(cnt[0], \
 			'(UID BODY[TEXT])')
 
-
 print dta[0][1]
 mserver.close()
 mserver.logout()
 text_file = open("Email.txt", "w")
 text_file.write(dta[0][1])
 text_file.close()
-if not 'Subject: Sign into Renegade Line' in open('Email.txt').read():
-    print("false")
+if not 'Have fun gaming!' in open('Email.txt').read():
+    print("Wrong Email")
     text_file = open("error.txt", "w")
     text_file.write("Your first email isn't the email from Raw Vengeance Games containing the code")
     text_file.close()
     variable1 = 1
+    input()
    
 
 text_file = open("Key.txt", "w")
-text_file.write(var1)
+text_file.write("Here is your key:\n")
+text_file.write(open("Email.txt", "r").readlines()[9])
+#text_file.write(var1)
 text_file.close()
 if variable1 == 1:
     os.remove("Key.txt")
+else:
+    print(open("Key.txt", "r").read())
+input()
